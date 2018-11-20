5952a88f3750354f7628b0e3b2b8b3522e669f19
Michal Siwek <siwekmichal21@gmail.com>
2018-11-21 00:40:26 +0100

    RequestDataRepo

--- TravelBlog/Controllers/HomeController.cs
+++ TravelBlog/Controllers/HomeController.cs
@@ -1,15 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
 using System.Web;
 using System.Web.Mvc;
+using Newtonsoft.Json;
+using TravelBlog.Models;
+using TravelBlog.Repository;
 
 namespace TravelBlog.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly RequestDataRepository _requestDataRepository;
+
+        public HomeController() => _requestDataRepository = new RequestDataRepository();
+
         public ActionResult Index()
         {
+            var top3 = _requestDataRepository.GetTop3EntriesByCreateDate();
+
             return View();
         }
 
---
+++ TravelBlog/Repository/RequestDataRepository.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Web;
+using Newtonsoft.Json;
+using TravelBlog.Infrastructure;
+using TravelBlog.Models;
+
+namespace TravelBlog.Repository
+{
+    public interface IRequestDataRepository
+    {
+        List<T> GetElements<T>();
+        T GetElement<T>(int id);
+        List<Entry> GetTop3EntriesByCreateDate();
+    }
+
+    public class RequestDataRepository : IRequestDataRepository
+    {
+        private readonly string _serverAddress;
+
+        private readonly IRequestService _requestService;
+
+        public RequestDataRepository()
+        {
+            _serverAddress = "http://localhost:49743/api";
+            _requestService = new RequestService();
+        } 
+
+        public List<T> GetElements<T>()
+        {
+            using (var client = new WebClient())
+            {
+                var elementType = _requestService.GetTypeRoute(typeof(T).Name);
+                var requestsData = client.DownloadData($"{_serverAddress}/{elementType}");
+
+                string jsonStr = Encoding.UTF8.GetString(requestsData);
+                var output = JsonConvert.DeserializeObject<List<T>>(jsonStr);
+
+                return output;
+            }
+        }
+
+        public T GetElement<T>(int id)
+        {
+            using (var client = new WebClient())
+            {
+                var elementType = _requestService.GetTypeRoute(typeof(T).Name);
+                var requestsData = client.DownloadData($"{_serverAddress}/{elementType}/{id}");
+
+                string jsonStr = Encoding.UTF8.GetString(requestsData);
+                var output = JsonConvert.DeserializeObject<T>(jsonStr);
+
+                return output;
+            }
+        }
+
+        public List<Entry> GetTop3EntriesByCreateDate()
+        {
+            using (var client = new WebClient())
+            {
+                var elementType = _requestService.GetTypeRoute(typeof(Entry).Name);
+                var requestsData = client.DownloadData($"{_serverAddress}/{elementType}");
+
+                string jsonStr = Encoding.UTF8.GetString(requestsData);
+                var output = JsonConvert.DeserializeObject<List<Entry>>(jsonStr)
+                    .OrderByDescending(p => p.CreateDate)
+                    .Take(3)
+                    .ToList();
+
+                return output;
+            }
+        }
+
+    }
+}
