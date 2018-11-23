96f4804a7f47303103c7f49aa5ffe1e091d24ae4
Michal Siwek <siwekmichal21@gmail.com>
2018-11-23 20:27:11 +0100

    Models

---
+++ TravelBlog/Models/Viewmodels/GalleriesViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TravelBlog.Models.Viewmodels
+{
+    public class GalleriesViewModel
+    {
+        public IEnumerable<Gallery> Galleries { get; set; }
+    }
+}
