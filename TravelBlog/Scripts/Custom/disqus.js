860e35143c815e5142624854239bdf183da2c6c4
Michal Siwek <michal.siwek@ceneo.pl>
2018-12-07 16:25:15 +0100

    Disqus fix

--- TravelBlog/Scripts/Custom/disqus.js
+++ TravelBlog/Scripts/Custom/disqus.js
@@ -1,10 +1,11 @@
 var disqus_url = 'http://localhost/';
-/**
-*  RECOMMENDED CONFIGURATION VARIABLES: EDIT AND UNCOMMENT THE SECTION BELOW TO INSERT DYNAMIC VALUES FROM YOUR PLATFORM OR CMS.
-*  LEARN WHY DEFINING THESE VARIABLES IS IMPORTANT: https://disqus.com/admin/universalcode/#configuration-variables*/
+
+/*
+    *  RECOMMENDED CONFIGURATION VARIABLES: EDIT AND UNCOMMENT THE SECTION BELOW TO INSERT DYNAMIC VALUES FROM YOUR PLATFORM OR CMS.
+    *  LEARN WHY DEFINING THESE VARIABLES IS IMPORTANT: https://disqus.com/admin/universalcode/#configuration-variables
+*/
 
 var disqus_config = function () {
-    console.log(window.location.href);
     this.page.url = window.location.href;  // Replace PAGE_URL with your page's canonical URL variable
     this.page.identifier = window.location.href + '#'; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
 };
